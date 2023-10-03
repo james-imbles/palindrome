ed715a972865796aba9da47f1983a55e12a7f25d
James <james@imbles.com>
2023-10-03 12:49:46 +0100

    Added in reactive states for button group

--- assets/Pages/Home.vue
+++ assets/Pages/Home.vue
@@ -1,5 +1,9 @@
 <script setup>
+import { ref } from 'vue'
 import AuthenticatedLayout from "../Layouts/AuthenticatedLayout.vue";
+
+const mode = ref('palindrome');
+
 </script>
 
 <template>
@@ -10,9 +14,18 @@ import AuthenticatedLayout from "../Layouts/AuthenticatedLayout.vue";
     </template>
     <div>
       <div class="flex w-max my-6 mx-auto font-bold bg-green-500 rounded-lg">
-        <button class="hover:bg-green-200 w-100">Palindrome</button>
-        <button class="hover:bg-green-200">Anagram</button>
-        <button class="hover:bg-green-200">Pangram</button>
+        <button 
+         @click="mode = 'palindrome'"
+        :class="`${mode === 'palindrome' ?  'bg-orange-500': ''}`"
+        class="rounded-l-lg p-2">Palindrome</button>
+        <button 
+          @click="mode = 'anagram'"
+         :class="`${mode === 'anagram' ?  'bg-orange-500': ''}`"
+        class="p-2">Anagram</button>
+        <button
+          @click="mode = 'panagram'" 
+         :class="`${mode === 'panagram' ?  'bg-orange-500': ''}`"
+        class="rounded-r-lg p-2">Pangram</button>
       </div>
       <div class="flex w-max mx-auto">
         <textarea class="border border-orange-500 h-96 w-96 rounded-xl dark:bg-gray-900 p-12 transition"></textarea>
